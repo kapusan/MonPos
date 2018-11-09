b22194e903da93d54957bb97a0f7fdda349e66a3
Anang S <afirstlive@gmail.com>
2018-11-09 22:08:46 +0700

    first commit

---
+++ app/src/main/java/id/rackspira/seedisaster/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.rackspira.seedisaster
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
